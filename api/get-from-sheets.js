8741be9fe3aafab48d94f7648aa9a1280899477f
MiniTrencher <manager@minitrencher.com>
2024-06-27 15:49:55 -0700

    Adds CORS

--- api/get-from-sheets.js
+++ api/get-from-sheets.js
@@ -7,6 +7,10 @@ const jsonCredentials = Buffer.from(base64Credentials, 'base64').toString('utf8'
 
 // Function to get data from a specific cell
 export const getCellData = async (range) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://office.minitrencher.com'); // Update with your frontend's URL
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     const auth = new google.auth.GoogleAuth({
         credentials: JSON.parse(jsonCredentials),
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
